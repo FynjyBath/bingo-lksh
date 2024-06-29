ce28b17ef2f0b6385ba4248fb340d280e9d1844a
FynjyBath <anton5-206@mail.ru>
2024-06-29 22:42:17 +0400

    Update main.go

--- main/main.go
+++ main/main.go
@@ -125,6 +125,7 @@ func GetTable(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Fprintf(w, string(jsonData))
 }
 
